33950324a87cc177d88cad43b1c1fac26edc59fb
kevin <kevin@kevin-PC>
2016-04-21 17:06:53 -0300

    casi casi

--- src/pkg10285/Main.java
+++ src/pkg10285/Main.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 class Main
 {
-    static String ReadLn (int maxLg)  // utility function to read from stdin
+    static String ReadLn (int maxLg)  
     {
         byte lin[] = new byte [maxLg];
         int lg = 0, car = -1;
@@ -25,27 +25,87 @@ class Main
             return (null);
         }
 
-        if ((car < 0) && (lg == 0)) return (null);  // eof
+        if ((car < 0) && (lg == 0)) return (null);  
         return (new String (lin, 0, lg));
     }
 
-    public static void main (String args[])  // entry point from OS
+    public static void main (String args[])  
     {
-        Main myWork = new Main();  // create a dinamic instance
-        myWork.Begin();            // the true entry point
+        Main myWork = new Main();  
+        myWork.Begin();            
     }
-
+    
+    private static int dimR, dimC;
+    private static int[][] matriz = new int[100+3][100+3];
+    private static int[][] dp = new int[100+3][100+3];
+    
     void Begin()
     {
-        String input;
+        String input, nombre = "";
         StringTokenizer idata;
-        int a, b, min, max, num, n, cycle, cyclemax;
-
-        while ((input = Main.ReadLn (255)) != null)
-        {
-          idata = new StringTokenizer (input);
-          a = Integer.parseInt (idata.nextToken());
-          
+        int casos = 0, contPrincipal = 1, cont = 0;
+        
+        input = Main.ReadLn (255);
+        idata = new StringTokenizer (input);
+        casos = Integer.parseInt (idata.nextToken());
+        
+        while ((input = Main.ReadLn (255)) != null){   
+            if(contPrincipal == 1){
+                idata = new StringTokenizer (input);
+                
+                nombre = idata.nextToken();
+                
+                dimR = Integer.parseInt (idata.nextToken());
+                
+                dimC = Integer.parseInt (idata.nextToken());
+                
+            }else if(contPrincipal == 2){
+                for(int i=0 ; i < dimR ; i++){
+                    if(i!=0){
+                        input = Main.ReadLn (255);
+                    }
+                    idata = new StringTokenizer (input);
+                    for(int j=0 ; j < dimC ; j++){
+                        matriz[i][j] = Integer.parseInt (idata.nextToken());                        
+                    }
+                }
+                
+                int max = -2147483647;
+                
+                for(int i = 0 ; i<dimR ; i++){
+                    for(int j = 0 ; j<dimC ; j++){
+                        int len = encontrar(i,j);
+                        if(len > max){
+                            max = len;
+                        }
+                    }
+                }
+                System.out.println(nombre+": "+max);
+            }
+            cont++;
+            contPrincipal ++;
+            if(contPrincipal == 3) contPrincipal = 1;
         }
+    }
+    
+    int encontrar(int x, int y){
+        if ( dp [x] [y] != -1 ) return dp [x] [y];
+
+        int up = 0, down = 0, right = 0, left = 0;
+
+        if ( x != 0 && matriz [x - 1] [y] < matriz [x] [y] )
+            up = encontrar (x - 1, y) + 1;
+
+        if ( x != dimR - 1 && matriz [x + 1] [y] < matriz [x] [y] ) 
+            down = encontrar (x + 1, y) + 1;
+
+        if ( y != 0 && matriz [x] [y - 1] < matriz [x] [y] ) 
+            left = encontrar (x, y - 1) + 1;
+
+        if ( y != dimC - 1 && matriz [x] [y + 1] < matriz [x] [y] ) 
+            right = encontrar (x, y + 1) + 1;
+
+        return dp [x] [y] = Math.max (up, Math.max (down, Math.max (right, left)));
+
     }
 }
